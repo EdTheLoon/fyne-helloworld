2fbe4855c8b3870b79d33b95069c858587615325
Adrian Reid <devilred101@gmail.com>
2024-06-21 02:56:23 +0100

    Changed to a more readable structure. Added a header label, separator and added an icon to the button

--- main.go
+++ main.go
@@ -3,27 +3,38 @@ package main
 import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	// Initialise the application
-	a := app.New()
+	// Initialise the application with a unique app ID
+	a := app.NewWithID("scot.reids.hellofyne")
 
 	// Initialise the window and set the title
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("Hello!")
+
+	// Header label
+	header := widget.NewRichTextFromMarkdown("# Hello, World!\n\nvia Fyne!")
+
+	// Separator
+	sep := widget.NewSeparator()
 
 	// Create a label widget
-	hello := widget.NewLabel("Hello, Fyne!")
+	hello := widget.NewLabel("This was created using Fyne! Try saying Hi!")
 
-	// Add the label to the window
-	w.SetContent(container.NewVBox( // Create a vertical box container
-		hello, // First element is the label
-		widget.NewButton("Hi!", // Second element is a button. Set button text here
-			func() { // Button's event handler. Can also call a function written elsewhere
-				hello.SetText("Welcome :)")
-			}),
-	)) // End of container.NewVBox and w.SetContent
+	// Create a button with text "Hi!" and a ConfirmIcon then define a function
+	// that runs when the button is clicked!
+	button := widget.NewButtonWithIcon("Hi!", theme.ConfirmIcon(), func() {
+		hello.SetText("Hello :)")
+	})
+	button.IconPlacement = widget.ButtonIconTrailingText
+
+	// Use a VBox to lay out the elements
+	vbox := container.NewVBox(header, sep, hello, button)
+
+	// Add the VBox and included elements to the window.
+	w.SetContent(vbox)
 
 	// Show and run the event handling for the window
 	// TODO: Research whether this can run concurrently?
